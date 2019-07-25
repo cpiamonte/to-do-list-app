e594312f442533a6317f8f012c9a81b98370098b
cpiamonte <claupiamonte26@gmail.com>
2019-07-25 11:27:20 +0800

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'to-do-list-app';
+  tasks = [];
+  taskName = '';
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+  }
+
+  onAddItem() {
+    this.tasks.push(this.taskName);
+    this.taskName = null;
+  }
+
+  onKeydown() {
+    if (this.taskName !== '') {
+      this.tasks.push(this.taskName);
+    }
+  }
+
+  onRemoveItem(i) {
+    this.tasks.splice(i, 1);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import {MatButtonModule, MatCheckboxModule, MatFormFieldModule, MatInputModule, MatListModule, MatToolbarModule, MatIconModule, MatCardModule} from '@angular/material';
+
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    MatListModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
